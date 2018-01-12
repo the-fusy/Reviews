73c286f185d5d88e6c39377e431be28f1515d93f
The_Fusy <the@fusy.ru>
2018-01-13 01:09:38 +0300

    add templates

--- A-6475.cpp
+++ A-6475.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 
+template<typename T>
 struct Node {
-    int value;
-    Node* next;
+    T value;
+    Node<T>* next;
 
-    Node(int val) : value(val), next(nullptr) {}
+    Node(T val) : value(val), next(nullptr) {}
 };
 
+template<typename T>
 struct List {
-    Node* root;
-    Node* last;
+    Node<T>* root;
+    Node<T>* last;
     int size;
     
     List() : root(nullptr), last(nullptr), size(0) {}
 
     List(List& list) : root(nullptr), last(nullptr), size(0) {
-        Node* now = list.root;
+        Node<T>* now = list.root;
         for (int i = 0; i < list.size; ++i) {
             addElement(now->value);
             now = now->next;
@@ -23,7 +25,7 @@ struct List {
     }
 
     List(List list, int l, int r) : root(nullptr), last(nullptr), size(0) {
-        Node* now = list.root;
+        Node<T>* now = list.root;
         for (int i = 0; i < list.size; ++i) {
             if (i >= r) break;
             if (i >= l) {
@@ -34,7 +36,7 @@ struct List {
     }
 
     void addElement(int val) {
-        Node* node = new Node(val);
+        Node<T>* node = new Node<T>(val);
         if (last == nullptr) {
             root = node;
             last = node;
@@ -46,7 +48,7 @@ struct List {
     }
 
     ~List() {
-        Node* now = root;
+        Node<T>* now = root;
         while (now != nullptr) {
             now = now->next;
             delete root;
@@ -55,15 +57,16 @@ struct List {
     }
 };
 
-List& mergeSort(List& v, int l, int r) {
+template<typename T>
+List<T>& mergeSort(List<T>& v, int l, int r) {
     if (r - l == 1) return v;
-    List list1(v, l - l, (r + l) / 2 - l);
-    List list2(v, (r + l) / 2 - l, r - l);
-    List sortedList1 = mergeSort(list1, l, (r + l) / 2);
-    List sortedList2 = mergeSort(list2, (r + l) / 2, r);
-    Node* it1 = sortedList1.root;
-    Node* it2 = sortedList2.root;
-    Node* now = v.root;
+    List<T> list1(v, l - l, (r + l) / 2 - l);
+    List<T> list2(v, (r + l) / 2 - l, r - l);
+    List<T> sortedList1 = mergeSort(list1, l, (r + l) / 2);
+    List<T> sortedList2 = mergeSort(list2, (r + l) / 2, r);
+    Node<T>* it1 = sortedList1.root;
+    Node<T>* it2 = sortedList2.root;
+    Node<T>* now = v.root;
     while (it1 != nullptr || it2 != nullptr) {
         if (it1 == nullptr) {
             now->value = it2->value;
@@ -84,7 +87,7 @@ List& mergeSort(List& v, int l, int r) {
 }
 
 int main() {
-    List list;
+    List<int> list;
     int n;
     std::cin >> n;
     for (int i = 0; i < n; ++i) {
@@ -93,7 +96,7 @@ int main() {
         list.addElement(temp);
     }
     list = mergeSort(list, 0, n);
-    Node* now = list.root;
+    Node<int>* now = list.root;
     for (int i = 0; i < n; ++i) {
         std::cout << now->value << ' ';
         now = now->next;
